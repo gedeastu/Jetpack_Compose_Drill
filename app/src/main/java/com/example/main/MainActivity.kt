0adef63c562b3954a1cf260e66064e8de47326fb
gedeastu <gede.astu@gmail.com>
2024-01-29 14:39:06 +0800

    update about rows and column

--- app/src/main/java/com/example/main/MainActivity.kt
+++ app/src/main/java/com/example/main/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.main.ui.theme.MainTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +25,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MainTheme {
+                RowsColumnsAndBasicSizing()
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    //Greeting("Android")
+//                }
             }
         }
     }
@@ -36,6 +45,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+//Rows, Columns & Basic Sizing
+@Composable
+fun RowsColumnsAndBasicSizing(){
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly) {
+        Column(modifier = Modifier) {
+            Text(text = "This is Column")
+            Text(text = "Hello")
+            Text(text = "Kontol")
+        }
+        Row(modifier = Modifier) {
+            Text(text = "This is Row")
+            Spacer(modifier = Modifier.padding(horizontal = 5.dp))
+            Text(text = "Hello")
+            Text(text = "Kontol")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
